2a8c34b2a0a922fb37eff9522d00b5f2a6f4d82d
Todd Donaldson <todd.donaldson22@gmail.com>
2025-05-16 12:43:39 -0700

    initial test for playwright

---
+++ AutomationFrameworkRepo_v03/Src/Fixtures/BaseTestFixture.cs
@@ -0,0 +1,21 @@
+﻿using System.IO;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+
+public class PlaywrightFixture
+{
+    private readonly JObject _config;
+
+    public PlaywrightFixture()
+    {
+        var projectRoot = Directory.GetParent(Directory.GetCurrentDirectory()).Parent.Parent.FullName;
+        var configPath = Path.Combine(projectRoot, "Config", "Portal", "locators.json");
+        var configContent = File.ReadAllText(configPath);
+        _config = JsonConvert.DeserializeObject<JObject>(configContent);
+    }
+
+    public string GetSelector(string page, string element)
+    {
+        return _config[page]?[element]?.ToString() ?? throw new KeyNotFoundException($"Selector for {page}.{element} not found.");
+    }
+}
