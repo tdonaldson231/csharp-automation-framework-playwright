d79208dcfd642b0e7c6f35196e6a7109741dd733
Todd Donaldson <todd.donaldson22@gmail.com>
2025-05-28 14:09:49 -0700

    added no-cache to pipeline build

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,4 +2,4 @@
 dotnet test AutomationFrameworkRepo_v03.sln \
     --settings Tests/RunSettings/"${TEST_ENV}".runsettings \
     --filter "TestCategory=${TEST_CATEGORY}" \
-    --logger "trx;LogFileName=test_results.trx"
+    --logger "trx;LogFileName=/app/TestResults/test_results.trx"
