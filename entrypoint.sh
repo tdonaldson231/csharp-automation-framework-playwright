382b25547e0c3e17d5baf37df82ab0b5fa8d7261
Todd Donaldson <todd.donaldson22@gmail.com>
2025-05-28 13:46:50 -0700

    entrypoint script for new script location

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,4 +2,4 @@
 dotnet test AutomationFrameworkRepo_v03.sln \
     --settings Tests/RunSettings/"${TEST_ENV}".runsettings \
     --filter "TestCategory=${TEST_CATEGORY}" \
-    --logger "trx;LogFileName=Tests/Results/test_results.trx"
+    --logger "trx;LogFileName=test_results.trx"
