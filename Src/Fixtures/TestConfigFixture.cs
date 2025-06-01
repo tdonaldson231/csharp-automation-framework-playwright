b77b5f00b8db4b19e9639362263debe0cb344017
Todd Donaldson <todd.donaldson22@gmail.com>
2025-06-01 13:41:16 -0700

    updating pipeline to run presetup step for docker compose

--- Src/Fixtures/TestConfigFixture.cs
+++ Src/Fixtures/TestConfigFixture.cs
@@ -1,6 +1,7 @@
 ﻿public class TestConfigFixture
 {
     public string TestEnvironment { get; }
+    public string DbServer { get; }
     public string RestApiUrl { get; }
     public string MySqlConnection { get; }
     public string CurrentWorkingDir { get; }
@@ -9,8 +10,10 @@
     public TestConfigFixture()
     {
         TestEnvironment = TestContext.Parameters["testEnvironment"] ?? "dev";
+        DbServer = TestContext.Parameters["dbServer"] ?? "mysql";
+
         RestApiUrl = $"https://api.restful-api.{TestEnvironment}";
-        MySqlConnection = $"Server=mysql;Port=3306;Database={TestEnvironment}db;User ID={TestEnvironment}user;Password={TestEnvironment}password;";
+        MySqlConnection = $"Server={DbServer};Port=3306;Database={TestEnvironment}db;User ID={TestEnvironment}user;Password={TestEnvironment}password;";
         CurrentWorkingDir = Directory.GetCurrentDirectory();
         ProjectPath = Directory.GetParent(CurrentWorkingDir)?.Parent?.Parent?.FullName
                       ?? throw new DirectoryNotFoundException("Unable to resolve project root.");
