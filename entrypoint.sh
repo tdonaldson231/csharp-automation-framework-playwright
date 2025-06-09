bfafda4eeac0b2af3618bbba08aed151a7c865f3
Todd Donaldson <todd.donaldson22@gmail.com>
2025-06-09 15:08:33 -0700

    updated the script for the html report

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
+
 dotnet test AutomationFrameworkRepo_v03.sln \
     --settings Tests/RunSettings/"${TEST_ENV}".runsettings \
     --filter "TestCategory=${TEST_CATEGORY}" \
     --logger "trx;LogFileName=test_results.trx"
+
+# After tests finish, copy the generated HTML report to the mounted TestResults folder
+cp /app/Reports/ExtentReport_*.html /app/TestResults/
